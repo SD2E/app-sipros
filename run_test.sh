e2b25d11a949a69ca6f50c8d5b9d6233e38fa3c9
wjallen <wallen@tacc.utexas.edu>
2021-01-13 14:25:42 -0600

    removing secrets

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,1 @@
-curl -X POST --data "@job.json" -H "Content-Type:application/json" -H "Authorization:Bearer ***REMOVED***" https://api.sd2e.org/jobs/v2?pretty=true
+curl -X POST --data "@job.json" -H "Content-Type:application/json" -H "Authorization:Bearer **REMOVED**" https://api.sd2e.org/jobs/v2?pretty=true
